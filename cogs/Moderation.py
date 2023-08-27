b37cd0ee0c2f63f3ca1fd2109f8a46d6b489e36d
Ohhturnz <turnz7885@gmail.com>
2023-08-27 13:46:44 +0200

    Añadimos funcionalidad de renombrar

--- cogs/Moderation.py
+++ cogs/Moderation.py
@@ -31,7 +31,7 @@ class Moderation(commands.Cog, name = "Moderation"):
                         value= reason
                     )
                     await member.kick(reason= reason)
-                    await member.send(f"Bro did u know this boi {context.author} has kicked u wtf he told me it was cuz this reason: {reason}")
+                    await member.send(f"Bro did u know this boi {context.author} has kicked u? Just sayin' that he told me it was cuz this reason: {reason}")
             except Exception:
                 await context.send("There was an unexpected error")
         else:
@@ -49,7 +49,7 @@ class Moderation(commands.Cog, name = "Moderation"):
         member = await context.guild.fetch_member(user.id)
         if context.message.author.guild_permissions.manage_nicknames:
             try:
-                member.edit(nick = nick)
+                await member.edit(nick = nick)
                 embed = discord.Embed(
                     title="Succesfully changed user's nickname",
                     description=f"Say hi <@{member.id}>",
